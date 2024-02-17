d78d1e39d99aba6a28e63a5e8e946410c586872d
alaa.khalil <alaakhalil498@gmail.com>
2024-02-17 13:35:15 +0200

    "image done "

--- src/main/java/com/example/Ecommerce_project/Controller/AdminController.java
+++ src/main/java/com/example/Ecommerce_project/Controller/AdminController.java
@@ -6,14 +6,18 @@ import com.example.Ecommerce_project.Models.Categories;
 import com.example.Ecommerce_project.Models.Products;
 import com.example.Ecommerce_project.Services.CategoryService;
 import com.example.Ecommerce_project.Services.ProductService;
+import com.example.Ecommerce_project.dto.CategoryDto;
 import com.example.Ecommerce_project.dto.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @org.springframework.stereotype.Controller
@@ -47,7 +51,7 @@ public class AdminController {
     //http://localhost:8092/manageCategories
 @GetMapping("/manageCategories")
 public String showCategories(ModelMap modelMap){
-        List<Categories>cate=categoryService.getAllCategories();
+        List<CategoryDto> cate=categoryService.getAllCategories();
         modelMap.addAttribute("categories",cate);
         return "CategoriesPage";
   }
@@ -62,7 +66,7 @@ public String showCategories(ModelMap modelMap){
 @PostMapping("/saveCategory")
 public String saveCategory(@ModelAttribute("categories")Categories categories,ModelMap modelMap){
         categoryService.saveCategories(categories);
-    List<Categories>cate=categoryService.getAllCategories();
+    List<CategoryDto>cate=categoryService.getAllCategories();
     modelMap.addAttribute("categories",cate);
         return "CategoriesPage";
 }
@@ -72,7 +76,7 @@ public String saveCategory(@ModelAttribute("categories")Categories categories,Mo
 public String deleteCategory(@RequestParam("id") int id , ModelMap modelMap){
         Categories categories=categoryService.getById(id);
          categoryService.deleteCategory(categories);
-         List<Categories>cate=categoryService.getAllCategories();
+         List<CategoryDto>cate=categoryService.getAllCategories();
          modelMap.addAttribute("categories",cate);
          return "CategoriesPage";
 }
@@ -88,7 +92,7 @@ public String mangeProducts(ModelMap modelMap,ModelMap modelMap2){
 
     @GetMapping ("/addProduct")
     public String addProduct(ModelMap modelMap){
-        List<Categories>cate=categoryService.getAllCategories();
+        List<CategoryDto>cate=categoryService.getAllCategories();
         modelMap.addAttribute("categories",cate);
         return "AddProducts";
     }
@@ -104,8 +108,14 @@ public String mangeProducts(ModelMap modelMap,ModelMap modelMap2){
     }
 
 
-
-
+    //http://localhost:8092/showPhoto
+    @GetMapping(value = "/showPhoto")
+    public String showPhoto(ModelMap modelMap) throws IOException {
+        byte[] image = productService.downloadProductPhoto(6);
+        String base64Image = Base64.getEncoder().encodeToString(image);
+        modelMap.addAttribute("image", base64Image);
+        return "Photos";
+    }
 
 
 }
--- src/main/java/com/example/Ecommerce_project/Services/ProductService.java
+++ src/main/java/com/example/Ecommerce_project/Services/ProductService.java
@@ -3,10 +3,24 @@ package com.example.Ecommerce_project.Services;
 import com.example.Ecommerce_project.Models.Products;
 import com.example.Ecommerce_project.dto.ProductDto;
 import com.example.Ecommerce_project.repos.ProductRepo;
+import jakarta.annotation.Resource;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +32,11 @@ public class ProductService {
 
     @Autowired
     public ModelMapper modelMapper;
+
+    @Value("${products.image.directory}")
+    private String imageDirectory;
+
+
     public ProductService(ProductRepo productRepo) {
         this.productRepo = productRepo;
     }
@@ -46,5 +65,18 @@ public class ProductService {
         ProductDto productDto=convertToDto(products);
         return productDto;
    }
+
+
+
+
+    public byte[] downloadProductPhoto(Integer id) throws IOException {
+        Products products = productRepo.findById(id).get();
+        File photoFile = new File(products.getImageData());
+
+        byte[] photoData = Files.readAllBytes(photoFile.toPath());
+
+        return  photoData;
+
+    }
 
 }
